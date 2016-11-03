6a6b529a6131d1615f9ffe05f4a2a5a4b6ea1fdf
Kevin Smith <kvsm81@gmail.com>
2016-11-03 23:30:00 +0000

    Completed chapter 2

--- chapter2/src/Main.purs
+++ chapter2/src/Main.purs
@@ -1,10 +1,10 @@
 module Main where
 
-import Prelude ((+), (*))
+import Prelude
 import Control.Monad.Eff.Console (logShow)
-import Math (sqrt)
+import Math (pi, sqrt)
 
-diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
+circleArea r = pi * r * r
 
 main = logShow (diagonal 3.0 4.0)

54b3bf7abce2edaffacfcbdcfe043b92d912a6d9
Kevin Smith <kvsm81@gmail.com>
2016-11-07 21:10:51 +0000

    Fix bug in binomial coefficient

--- chapter5/src/exercises.purs
+++ chapter5/src/exercises.purs
@@ -8,7 +8,7 @@ factorial n = fact' n 0 where
   fact' n acc = fact' (n - 1) (acc + n)
 
 binomial :: Int -> Int -> Int
-binomial 0 n = 0
+binomial 0 n = 1
 binomial k 0 = 0
 binomial k n  | k == n    = 1
               | otherwise = (binomial (k - 1) (n - 1)) + (binomial k (n - 1))

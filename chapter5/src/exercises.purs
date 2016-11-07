c2cc03e69919889c84e8f860d133cdb664c9b9d3
Kevin Smith <kvsm81@gmail.com>
2016-11-07 20:30:37 +0000

    Recursive binomial coefficient w/ pattern matching

--- chapter5/src/exercises.purs
+++ chapter5/src/exercises.purs
@@ -6,3 +6,9 @@ factorial :: Int -> Int
 factorial n = fact' n 0 where
   fact' 0 acc = acc
   fact' n acc = fact' (n - 1) (acc + n)
+
+binomial :: Int -> Int -> Int
+binomial 0 n = 0
+binomial k 0 = 0
+binomial k n  | k == n    = 1
+              | otherwise = (binomial (k - 1) (n - 1)) + (binomial k (n - 1))
